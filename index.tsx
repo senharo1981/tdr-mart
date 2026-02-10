8b54eba29d89b0aad97021bdaa47d5bc07256da8
senharo1981 <senharo1981@gmail.com>
2026-02-10 06:58:04 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
+
+console.log("TDR Mart: Application is starting...");
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Critical Error: Root element not found.");
+}
